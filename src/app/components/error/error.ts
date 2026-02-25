ce95484c9dded8ed3f2235aa849a5752e77f4014
AH-HESHAM <ahmedhesham1652001@gmail.com>
2026-02-25 15:44:46 +0200

    add error routing page

---
+++ src/app/components/error/error.ts
@@ -0,0 +1,16 @@
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-error',
+  imports: [],
+  templateUrl: './error.html',
+  styleUrl: './error.css',
+})
+export class Error {
+   constructor(private location:Location , private router:Router){}
+   BackToHome(){
+    this.router.navigateByUrl('/')
+   }
+}
