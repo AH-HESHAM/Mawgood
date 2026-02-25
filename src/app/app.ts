f7ea3d5482f2f3960262830ba45cc9977cc0a76f
AH-HESHAM <ahmedhesham1652001@gmail.com>
2026-02-25 14:33:38 +0200

    build main html structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Slider } from './components/slider/slider';
+import { Body } from './components/body/body';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Slider, Body, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
