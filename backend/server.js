24008977455df8c58a818f97c8295fd6409871e7
AH-HESHAM <ahmedhesham1652001@gmail.com>
2026-03-08 17:09:35 +0200

    connect backend to database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+const productRoutes = require("./routes/productRoutes")
+
+
 dotenv.config();
+
+connectDB();
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(
+  cors({
+    origin: process.env.FRONT_URL,
+  }),
+);
+
+app.use(express.json());
+
+app.use("/products", productRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
