3a20156b6fe2f5c0ccbef6fd67f8c581979ef4c1
josephShokry <josephshokry9@gamil.com>
2026-03-09 11:44:15 +0200

    add middelware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+function authMiddleware(req, res, next) {
+
+  if (
+    req.path === "/api/auth/login" ||
+    req.path === "/api/auth/register"
+  ) {
+    return next();
+  }
+
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+}
+
+module.exports = authMiddleware;
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const router = express.Router();
 
 const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes")
+const authMiddleware = require("./middlewares/authMiddleware");
 
 
 dotenv.config();
@@ -20,7 +22,7 @@ app.use(
     credentials: true,
   }),
 );
-
+app.use(authMiddleware);
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
@@ -31,3 +33,14 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+// for testing purposes only, remove in production
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.use("/api/users", router);
