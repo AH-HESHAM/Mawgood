158e188bd3413072db7fee4fd466167a5aa6978c
AH-HESHAM <ahmedhesham1652001@gmail.com>
2026-03-11 16:08:09 +0200

    delete unwanted files

--- frontend/src/app/models/json-data-getter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { JsonDataGetter } from './json-data-getter';
-
-describe('JsonDataGetter', () => {
-  it('should create an instance', () => {
-    expect(new JsonDataGetter()).toBeTruthy();
-  });
-});
