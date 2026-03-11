2a725215cac7443983d450c4ee65e0214a6532c4
AH-HESHAM <ahmedhesham1652001@gmail.com>
2026-03-11 17:02:02 +0200

    remove unwanted imports

--- frontend/src/app/components/filters/filters.ts
+++ frontend/src/app/components/filters/filters.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { DataGetter } from '../../models/data-getter';
-import { JsonDataGetter } from '../../models/json-data-getter';
 import { FormsModule } from '@angular/forms';
+import { DynamicData } from '../../services/dynamic-data';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-filters',
@@ -10,27 +10,36 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './filters.css',
 })
 export class Filters {
-  private dataGetter: DataGetter;
-  categories: string[];
+  categories: string[] = [];
   chosenCategory: string;
   searchText: string;
   minPrice: any;
   maxPrice: any;
-  priceErrMsg:string = "";
+  priceErrMsg: string = '';
 
   @Output() categoryEvent = new EventEmitter<string>();
   @Output() textEvent = new EventEmitter<string>();
   @Output() minPriceEvent = new EventEmitter<number>();
   @Output() maxPriceEvent = new EventEmitter<number>();
 
-  constructor() {
-    this.dataGetter = new JsonDataGetter();
-    this.categories = [...new Set(this.dataGetter.getData().map((p) => p.category))];
-
+  constructor(private dataGetter: DynamicData) {
     this.chosenCategory = 'all';
     this.searchText = '';
     this.minPrice = null;
     this.maxPrice = null;
+  }
+
+  ngOnInit() {
+    this.dataGetter
+      .filterProducts()
+      .pipe(
+        map((products) => {
+          return [...new Set(products.map((p) => p.category))];
+        }),
+      )
+      .subscribe((cats) => {
+        this.categories = cats;
+      });
   }
 
   informParent() {
@@ -40,13 +49,13 @@ export class Filters {
     if (tempMax == null) tempMax = Number.MAX_VALUE;
     if (tempMin == null) tempMin = 0;
     if (tempMin >= 0 && tempMax >= 0 && tempMax >= tempMin) {
-      this.priceErrMsg = "";
+      this.priceErrMsg = '';
       this.categoryEvent.emit(this.chosenCategory);
       this.textEvent.emit(this.searchText);
       this.minPriceEvent.emit(tempMin);
       this.maxPriceEvent.emit(tempMax);
-    }else{
-      this.priceErrMsg = "Invalid price limit"
+    } else {
+      this.priceErrMsg = 'Invalid price limit';
     }
   }
 }
